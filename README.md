81df8ccb61b97172c37e527f3f860e1bfa49c85e
arxcode40 <arxcode40@gmail.com>
2025-02-04 15:40:58 +0700

    add dist library

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# session-js
+# sessionStorage JS
+
+## Introduction
+sessionStorage library inspired by Laravel Session
+
+## Installation
+- Download: [session.min.js](https://raw.github.com/arxcode40/session-js/main/dist/session.min.js) | [session.esm.min.js](https://raw.github.com/arxcode40/session-js/main/dist/session.esm.min.js)
+- Include into your project
+    ```html
+    <script src="dist/session.min.js"></script>
+    ```
+- Or use ES6 module
+    ```javascript
+    import session from './dist/session.esm.min.js'
+    ```
+
+## Interacting With the Session
+
+### Retrieving Data
+
+```javascript
+value = session.get('key')
+value = session.get('key', 'default')
+```
+
+#### Retrieving All Session Data
+```javascript
+data = session.all()
+```
+
+#### Retrieving a Portion of the Session Data
+```javascript
+data = session.only(['username', 'email'])
+
+data = session.except(['username', 'email'])
+
+```
+#### Determining if an Item Exists in the Session
+```javascript
+if (session.has('users')) {
+    // ...
+}
+
+if (session.exists('users')) {
+    // ...
+}
+
+if (session.missing('users')) {
+    // ...
+}
+```
+
+### Storing Data
+
+```javascript
+session.put('key', 'value')
+```
+
+#### Pushing to Array Session Values
+
+```javascript
+session.push('user', {teams: 'developer'})
+```
+
+#### Retrieving and Deleting an Item
+
+```javascript
+session.pull('key', 'default')
+```
+
+#### Incrementing and Decrementing Session Values
+
+```javascript
+session.increment('count')
+
+session.increment('count', 2)
+
+session.decrement('count')
+
+session.decrement('count', 2)
+```
+
+### Deleting Data
+
+```javascript
+// Forget a single key...
+session.forget('name')
+ 
+// Forget multiple keys...
+session.forget(['name', 'status'])
+ 
+session.flush()
+```
---
+++ dist/session.js
@@ -0,0 +1,83 @@
+'use strict'
+
+const session = {
+	all: function() {
+		return this._retrieve()
+	},
+	decrement: function(key, decrementBy = 1) {
+		this.put(key, this.get(key, 0) - decrementBy)
+	},
+	except: function(keys) {
+		return this._retrieve(keys, 'except')
+	},
+	exists: function(key) {
+		return this.all().hasOwnProperty(key)
+	},
+	flush: function() {
+		sessionStorage.clear()
+	},
+	forget: function(keys) {
+		if (Array.isArray(keys)) {
+			keys.forEach(key => sessionStorage.removeItem(key))
+			return
+		}
+
+		sessionStorage.removeItem(keys)
+	},
+	get: function(key, defaultValue = null) {
+		return JSON.parse(sessionStorage.getItem(key)) || defaultValue
+	},
+	has: function(key) {
+		return this.get(key) !== null
+	},
+	increment: function(key, incrementBy = 1) {
+		this.put(key, this.get(key, 0) + incrementBy)
+	},
+	missing: function(key) {
+		return !this.has(key)
+	},
+	only: function(keys) {
+		return this._retrieve(keys, 'only')
+	},
+	pull: function(key, defaultValue = null) {
+		const data = this.get(key, defaultValue)
+		this.forget(key)
+		return data
+	},
+	push: function(key, value) {
+		const data = this.get(key)
+		
+		if (Array.isArray(data)) {
+			data.push(value)
+			this.put(key, data)
+		}
+	},
+	put: function(key, value) {
+		sessionStorage.setItem(key, JSON.stringify(value))
+	},
+	_retrieve(keys = null, include = null) {
+        let data = {}
+
+        for (let i = 0; i < sessionStorage.length; i++) {
+			const key = sessionStorage.key(i)
+			
+			switch (include) {
+				case 'only':
+					if (keys.includes(key)) {
+						data[key] = this.get(key)
+					}
+					break
+				case 'except':
+					if (!keys.includes(key)) {
+						data[key] = this.get(key)
+					}
+					break
+				default:
+					data[key] = this.get(key)
+					break
+			}
+		}
+
+        return data
+    }
+}
